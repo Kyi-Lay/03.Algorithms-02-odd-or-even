954fb2b40f7ec01527c4e26c3299817fe8bf770e
pannajota <jotapanna2014@gmail.com>
2024-05-03 17:44:44 +0630

    first commit

--- Unsolved/odd-or-even.js
+++ Unsolved/odd-or-even.js
@@ -1,5 +1,5 @@
-Return the string "odd" if `num` is an odd number
-Return the string "even" if `num` is an even number
+ //Return the string "odd" if `num` is an odd number
+ //Return the string "even" if `num` is an even number
 
 var oddOrEven = function(num) {
     if (num % 2 === 0) {
